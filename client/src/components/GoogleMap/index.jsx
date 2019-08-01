075e10a08d2413126206bf0a7330b010cf3bb50c
Michael's <mjlikre@gmail.com>
2019-08-01 11:48:37 -0700

    maps working

--- client/src/components/GoogleMap/index.jsx
+++ client/src/components/GoogleMap/index.jsx
@@ -1,33 +1,45 @@
-import React, { Component } from 'react';
-import GoogleMapReact from 'google-map-react';
+import React, {Component} from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-class MyClass extends Component {
-  constructor(props){
+export class MapContainer extends Component {
+  constructor(props) {
     super(props);
 
+
+    this.state = {
+      stores: [{lat: 47.49855629475769, lng: -122.14184416996333},
+        {latitude: 47.359423, longitude: -122.021071},
+        {latitude: 47.2052192687988, longitude: -121.988426208496},
+        {latitude: 47.6307081, longitude: -122.1434325},
+        {latitude: 47.3084488, longitude: -122.2140121},
+        {latitude: 47.5524695, longitude: -122.0425407}]
+    }
+  }
+
+  displayMarkers = () => {
+    return this.state.stores.map((store, index) => {
+      return <Marker key={index} id={index} position={{
+        lat: store.latitude,
+        lng: store.longitude
+      }}
+                     onClick={() => console.log("You clicked me!")} />
+    })
   }
 
   render() {
     return (
-      <GoogleMapReact
-        defaultCenter={this.props.center}
-        defaultZoom={this.props.zoom}
-        style={{height: '300px'}}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text={'Google Map'}
-        />
-      </GoogleMapReact>
+        <Map
+            google={this.props.google}
+            zoom={8}
+
+            initialCenter={{ lat: 47.444, lng: -122.176}}
+        >
+          {this.displayMarkers()}
+        </Map>
     );
   }
 }
-MyClass.defaultProps = {
-  center: {lat: 59.95, lng: 30.33},
-  zoom: 11
-};
 
-export default MyClass
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBAQv_VajKKxKQTr1tw-PpDaMhV30n_D-c'
+})(MapContainer);
