e73b93266d174d1331560fe5191555eba9f64dbb
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-08-01 14:54:04 -0700

    fixed more stuff

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-module.imports = {
+module.exports = {
     secret: 'gfggkdhdgkhfbjfdkfgbkdbgdfkgbkdfgbkdjfbgkjdfbgjk'
 }
--- services/passport.js
+++ services/passport.js
@@ -1,8 +1,8 @@
-const passport      = require('passport');
-const User          = require('./../models/User');
-const config        = require('./../config');
-const JwtStrategy   = require('passport-jwt').Strategy;
-const ExtractJwt    = require('passport-jwt').ExtractJwt;
+const passport = require('passport');
+const User = require('./../models/User');
+const config = require('../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const LocalStrategy = require('passport-local');
 
 // Create local strategy
@@ -10,25 +10,25 @@ const LocalStrategy = require('passport-local');
 // By default LocalStrategy it is expecting a username and a password
 const localOptions = { usernameField: 'email' };
 
-const localLogin = new LocalStrategy(localOptions, async (email, password, done) => {
+const localLogin = new LocalStrategy(localOptions, async(email, password, done) => {
 
-  try {
-    const user = await User.findOne({ email });
-    if(!user) {
-      return done(null, false);
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return done(null, false);
+        }
+
+        user.comparePassword(password, (err, isMatch) => {
+            if (err) return done(err);
+            if (!isMatch) {
+                return done(null, false);
+            }
+            return done(null, user);
+        });
+
+    } catch (e) {
+        done(e, false);
     }
-
-    user.comparePassword(password, (err, isMatch) => {
-      if(err) return done(err);
-      if(!isMatch) {
-        return done(null, false);
-      }
-      return done(null, user);
-    });
-
-  } catch(e) {
-    done(e, false);
-  }
 });
 
 
@@ -36,29 +36,29 @@ const localLogin = new LocalStrategy(localOptions, async (email, password, done)
 // We need to tell our strategy where to look for the token
 
 const jwtOptions = {
-  // Tells JWT strategy that whenever a request comes in
-  // and we want passport to handle it
-  // It needs to look in the header, for the property called "authorization"
-  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
-  // tells jwt strategy what secret we used to encode the token
-  // so that it can decode it
-  secretOrKey: config.secret
+    // Tells JWT strategy that whenever a request comes in
+    // and we want passport to handle it
+    // It needs to look in the header, for the property called "authorization"
+    jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+    // tells jwt strategy what secret we used to encode the token
+    // so that it can decode it
+    secretOrKey: config.secret
 };
 
 // We are going to get the payload argument from an incoming request
 // The payload argument is coming frm the function that we will create in authRoutes
 // done is the function we call once we tried to authenticate this user
-const jwtLogin = new JwtStrategy(jwtOptions, async (payload, done) => {
-  try {
-    const user = await User.findById(payload.sub);
-    if(user) {
-      done(null, user);
-    } else {
-      done(null, false);
+const jwtLogin = new JwtStrategy(jwtOptions, async(payload, done) => {
+    try {
+        const user = await User.findById(payload.sub);
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    } catch (e) {
+        done(e, false);
     }
-  } catch(e) {
-    done(e, false);
-  }
 });
 
 
