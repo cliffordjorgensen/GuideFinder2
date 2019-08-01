176719c7e46745a56806d66b195c87813c2e9a94
deepesh Nagarkoti <deepesh.nagarkoti@gmail.com>
2019-08-01 13:29:07 -0700

    Creating search file at src

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Search = () => {
     return (
-        <div class="input-group">
-            <input type="text" className="form-control" placeholder="Search term"/>
-                <button className="btn btn-outline-secondary" type="button">Filters</button>
-                <button className="btn btn-outline-secondary" type="button">Submit</button>
+        <div>
+   <div class="input-group">
+        <input type="text" class="form-control" id="search-term" placeholder="Search term" aria-label="traveler search term" aria-describedby="button-addon4"/>
+        <div class="input-group-append" id="button-addon4">
+            <button class="btn btn-outline-secondary" type="button">Filters</button>
+            <button class="btn btn-success" type="button">Submit</button>
         </div>
-
+    </div>
+</div>
     )
 }
 
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Search from '../components/Search/Search';
 
 export default ({ children }) => {
   return (
+  
+      
     <div>
-      {children}
+    {children}
     </div>
+  
   )
 };
