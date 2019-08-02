e2d386cf5ebb7531a40cb1a3b5c02dfe19939b47
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-08-02 10:56:40 -0700

    added search navbar map to app file

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import Maps from "./Maps";
+import NavBar from "../components//NavBar";
+import Search from "../components/Search/Search";
 export default ({ children }) => {
   return (
     <div>
+      <NavBar />
+      <Maps />
+      <Search />
       {children}
     </div>
-  )
+  );
 };
-
 
 /*Dont panic, I cleared out whatever that was in here, and transfered them to Maps 
 in containers. Then i hooked up Maps to the index.jsx in the root folder. //#endregion
@@ -16,9 +20,4 @@ sign in or sign up. When we open the page it will initially sent you to the welc
 and it will offer you the option to sign in or sign up, once you've signed in or signed up, 
 it will direct you to the map page*/
 
-      <div className = "container">
-
-      </div>
-
-
-
+// {/* <div className='container' />; */}
