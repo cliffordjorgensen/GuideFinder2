d51005f4a642f33987ebd0cbc8d46814834d7783
ahydorn <adamhydorn@gmail.com>
2019-08-02 11:55:25 -0700

    removed most unused, uncommented code

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,60 +1,104 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
+import React from 'react'
+import { Link } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+
 
 const index = () => {
-  return (
-    <div>
-      <nav className='navbar navbar navbar-expand-lg shadow-sm'>
-        <Link className='navbar-brand naturalWHite rounded p-2 ' to='/'>
-          <i className='fa fa-home' /> Home
-        </Link>
+    return (
+        <div>
+            <nav className="navbar navbar navbar-expand-lg shadow-sm">
+                <Link className="navbar-brand naturalWHite rounded p-2 " to="/">
+                    <i className="fa fa-home"></i> Home
+                </Link>
 
-        <div className='collapse navbar-collapse float-right' id='navbarText'>
-          <ul className='nav nav-pills'>
-            <li className='nav-item'>
-              <Link
-                className='nav-link float-right naturalWHite'
-                to='/profiles/'
-              >
-                <i className='fa fa-users' />
-                Profiles
-              </Link>
-            </li>
-            <li className='nav-item dropdown'>
-              <Link
-                className='nav-link dropdown-toggle naturalWHite '
-                data-toggle='dropdown'
-                to='#'
-                role='button'
-                aria-haspopup='true'
-                aria-expanded='false'
-              >
-                <i className='fa fa-sign-in' />
-                Log In
-              </Link>
-              <div className='dropdown-menu HighlightGreen'>
-                <Link className='dropdown-item guide' to='/logIn'>
-                  Guide
-                </Link>
-                <div role='separator' className='dropdown-divider' />
-                <Link className='dropdown-item traveler' to='/loginUser'>
-                  Traveler
-                </Link>
-              </div>
-            </li>
-            <li className='nav-item'>
-              <Link className='nav-link naturalWHite' to='/contactUs'>
-                <i className='fa fa-id-badge' />
-                Contact us
-              </Link>
-            </li>
-          </ul>
+                <div className="collapse navbar-collapse float-right" id="navbarText">
+                    <ul className="nav nav-pills">
+                        <li className="nav-item">
+                            <Link className="nav-link float-right naturalWHite" to="/profiles/">
+                                <i className="fa fa-users"></i>
+                                Profiles
+                            </Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <Link className="nav-link dropdown-toggle naturalWHite " data-toggle="dropdown" to="#" role="button"
+                                aria-haspopup="true" aria-expanded="false">
+                                    <i className="fa fa-sign-in"></i>
+                                    Log In
+                            </Link>
+                            <div className="dropdown-menu HighlightGreen">
+                                <Link className="dropdown-item guide" to="/logIn">
+                                    Guide
+                                    </Link>
+                            <div role="separator" className="dropdown-divider"></div>
+                                <Link className="dropdown-item traveler" to="/loginUser">
+                                    Traveler
+                                </Link>
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link naturalWHite" to="/contactUs">
+                            <i className="fa fa-id-badge"></i> 
+                            Contact us
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+
+
+                {/* <Link to="/profiles/">Profiles</Link>
+                <Link to="/logIn/">Log In</Link>
+                <Link to="/contactUs/">Contact Us</Link> */}
+            </nav>
         </div>
-      </nav>
-    </div>
-  );
-};
+    )
+}
 
-export default index;
+export default index
+
+/* <nav id="navbar-example2" className="navbar navbar navbar-expand-lg shadow-sm">
+                <a className="navbar-brand naturalWHite rounded p-2 " href="/home"> <i className="fa fa-home"></i> Home</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"><i className="fa fa-bars HighlightGreen"></i></span>
+                </button>
+                <div className="collapse navbar-collapse float-right" id="navbarText">
+                    <ul className="nav nav-pills">
+                        <li className="nav-item">
+                            <a className="nav-link float-right naturalWHite" href="/profiles"> <i className="fa fa-users"></i>
+                                Profiles</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle naturalWHite " data-toggle="dropdown" href="#" role="button"
+                                aria-haspopup="true" aria-expanded="false"> <i className="fa fa-sign-in"></i> Log In</a>
+                            <div className="dropdown-menu HighlightGreen">
+                                <a className="dropdown-item guide" href="/login">Guide</a>
+                                <div role="separator" className="dropdown-divider"></div>
+                                <a className="dropdown-item traveler" href="/loginUser">Traveler</a>
+
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link naturalWHite" href="/contactUs"> <i className="fa fa-id-badge"></i> Contact us</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav> */
+
+            // <nav>
+            //     <ul>
+            //         <li>
+            //             <Link to="/">Home</Link>
+            //         </li>
+            //         <li>
+            //             <Link to="/profiles/">Profiles</Link>
+            //         </li>
+            //         <li>
+            //             <Link to="/users/">Log In</Link>
+            //         </li>
+            //         <li>
+            //             <Link to="/contactUs/">Contact Us</Link>
+            //         </li>
+            //     </ul>
+            // </nav>
--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import Maps from './Maps';
-import NavBar from './Navbar';
-import Search from './Search';
+
+import React from "react";
+import Maps from "./Maps";
+import NavBar from "./Navbar";
+import Search from "./Search";
 export default ({ children }) => {
   return (
     <div>
@@ -13,9 +14,15 @@ export default ({ children }) => {
   );
 };
 
+
+
+
 /*Dont panic, I cleared out whatever that was in here, and transfered them to Maps 
 in containers. Then i hooked up Maps to the index.jsx in the root folder. //#endregion
 I also went in to change the Welcome page inside of containers for it to contain options to 
 sign in or sign up. When we open the page it will initially sent you to the welcome page, 
 and it will offer you the option to sign in or sign up, once you've signed in or signed up, 
-it will direct you to the map page -Michael*/
+it will direct you to the map page*/
+
+
+// {/* <div className='container' />; */}
