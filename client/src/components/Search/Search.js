709b1b7b916b37f8dfa42505aa3243250ef78ed9
deepesh Nagarkoti <deepesh.nagarkoti@gmail.com>
2019-08-05 10:27:21 -0700

    working on submit botton

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,18 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-const Search = props => {
-    return (
-        <div>
-   <div class="input-group">
-        <input type="text" class="form-control" id="search-term" placeholder="Search term" aria-label="traveler search term" aria-describedby="button-addon4"/>
-        <div class="input-group-append" id="button-addon4">
-            <button class="btn btn-outline-primary" type="button">Filters</button> {props.filters}
-            <button class="btn btn-outline-success" type="button">Submit</button> {props.submit}
-        </div>
-    </div>
-</div>
-    )
-}
 
-export default Search;
+
+export default class Search extends Component {
+    handleClick = (event) => {
+        event.preventDefault();
+        console.log('this is:', this);
+      }
+    render() {
+        console.log(this.props)
+        
+        return (
+            <div>
+                <div class="input-group">
+                    <input type="text" class="form-control" id="search-term" placeholder="Search term" aria-label="traveler search term" aria-describedby="button-addon4" />
+                    <div class="input-group-append" id="button-addon4">
+                        <button class="btn btn-outline-primary" type="button"  onClick={this.handleClick} >Filters</button>
+                        <button class="btn btn-outline-success" type="button"  onClick={this.handleClick} >Submit</button>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
