245ad72258381e2a4118cbd5bc9fb38bfe13c7a1
deepesh Nagarkoti <deepesh.nagarkoti@gmail.com>
2019-08-06 13:08:33 -0700

    Updating the detail info of each members

--- client/src/components/NavBar/index.jsx
+++ client/src/components/NavBar/index.jsx
@@ -1,57 +1,4 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Link } from "react-router-dom"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'font-awesome/css/font-awesome.min.css'
-// import ContactUs from './../ContactUs'
 
-const index = () => {
-    return (
-        <div>
-            <nav className="navbar navbar navbar-expand-lg shadow-sm">
-                <Link className="navbar-brand naturalWHite rounded p-2 " to="/">
-                    <i className="fa fa-home"></i> Home
-                </Link>
-
-                <div className="collapse navbar-collapse float-right" id="navbarText">
-                    <ul className="nav nav-pills">
-                        <li className="nav-item">
-                            <Link className="nav-link float-right naturalWHite" to="/profiles/">
-                                <i className="fa fa-users"></i>
-                                Profiles
-                            </Link>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <Link className="nav-link dropdown-toggle naturalWHite " data-toggle="dropdown" to="#" role="button"
-                                aria-haspopup="true" aria-expanded="false">
-                                    <i className="fa fa-sign-in"></i>
-                                    Log In
-                            </Link>
-                            <div className="dropdown-menu HighlightGreen">
-                                <Link className="dropdown-item guide" to="/logIn">
-                                    Guide
-                                    </Link>
-                            <div role="separator" className="dropdown-divider"></div>
-                                <Link className="dropdown-item traveler" to="/loginUser">
-                                    Traveler
-                                </Link>
-                            </div>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link naturalWHite" to="/ContactUs">
-                            <i className="fa fa-id-badge"></i> 
-                            Contact us
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-
-
-                {/* <Link to="/profiles/">Profiles</Link>
-                <Link to="/logIn/">Log In</Link>
-                <Link to="/contactUs/">Contact Us</Link> */}
-            </nav>
-=======
 import React from "react";
 import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -90,7 +37,6 @@ const index = () => {
               </Link>
             </li>
           </ul>
->>>>>>> b4c68cfb3f7cf1eb9a1641d0f67cb778e8b1965d
         </div>
       </nav>
     </div>
--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-<<<<<<< HEAD
+
 // import GoogleMap from '../components/GoogleMap';
 // import Search from '../components/Search/Search';
 
 
 
-export default ({ children }) => {
-=======
+
 export default ({children}) => {
->>>>>>> b4c68cfb3f7cf1eb9a1641d0f67cb778e8b1965d
   return (
     <div>
       <NavBar/>
