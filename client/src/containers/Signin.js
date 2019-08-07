2c463f35d5a828a858d6a0634b9f547c8637637e
Badri Vasistha <badri.vasistha@gmail.com>
2019-08-07 12:34:49 -0700

    few aesthetic changes in signin and signup page

--- client/src/containers/Signin.js
+++ client/src/containers/Signin.js
@@ -19,46 +19,60 @@ class Signin extends Component {
   };
 
   render() {
-
     const { handleSubmit } = this.props;
     return (
-      <form onSubmit={handleSubmit(this.onSubmit)}>
-        <fieldset className="my-fieldset">
-          <label>Email</label>
-          <Field
-            name="email"
-            type="text"
-            label="Email"
-            component={this.renderInput}
-            autoComplete="none"
-            className="form-control"
-            aria-describedby="emailHelp"
-          />
-        </fieldset>
-        <fieldset>
-          <label>Password</label>
-          <Field
-            name="password"
-            type="password"
-            label="password"
-            component={this.renderInput}
-            autoComplete="none"
-          />
-        </fieldset>
-        <button className="nav-link float-left naturalWHite"> <i className="fa fa-sign-in" aria-hidden="true" />
-          Signin
-          </button>
-        <div>{this.props.errorMessage}</div>
-        <br/>
-        <div>
-          <button>
-            <Link className="nav-link float-left naturalWHite" to="/guideloc">
-              <i className="fa fa-paper-plane" aria-hidden="true" />
-              Sign Up
-            </Link>
-          </button>
+      <div className="container mt-5">
+        <div className="row">
+          <div className="col-md-5 mx-auto">
+            <div id="first">
+              <div className="myform form ">
+                <form onSubmit={handleSubmit(this.onSubmit)}>
+                  <fieldset className="my-fieldset">
+                    <label>Email</label>
+                    <Field
+                      name="email"
+                      type="text"
+                      label="Email"
+                      component={this.renderInput}
+                      autoComplete="none"
+                      className="form-control"
+                      aria-describedby="emailHelp"
+                    />
+                  </fieldset>
+                  <fieldset>
+                    <label>Password</label>
+                    <Field
+                      name="password"
+                      type="password"
+                      label="password"
+                      component={this.renderInput}
+                      autoComplete="none"
+                    />
+                  </fieldset>
+                  <button className="btn btn-block tx-tfm navyBlue text-white">
+                    {" "}
+                    <i className="fa fa-sign-in" aria-hidden="true" />
+                    Signin
+                  </button>
+                  <div>{this.props.errorMessage}</div>
+                  <br />
+                  <div className="col-md-12 text-center ">
+                    <button className="btn btn-block tx-tfm navyBlue text-white">
+                      <Link
+                        className="nav-link float-left naturalWHite"
+                        to="/guideloc"
+                      >
+                        <i className="fa fa-paper-plane" aria-hidden="true" />
+                        Sign Up
+                      </Link>
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
-      </form>
+      </div>
     );
   }
 }
@@ -74,3 +88,4 @@ export default compose(
   ),
   reduxForm({ form: "signin" })
 )(Signin);
+
